54edb0e3b4489ba2bb0ffd198e74f44ea3798bc7
JoshuaKeys <avwerosuoghene.oguma1@gmail.com>
2023-01-06 16:03:03 +0200

    fixed errors

--- posts/index.js
+++ posts/index.js
@@ -1,30 +1,22 @@
 const express = require('express');
-// const connectDb = require('./db/connectDb');
-// const router = require('./routes/posts');
-// const { rabbitWrapper, connectRabbit } = require('@giantsoftnats/common')
-// const { connect } = require('./connectRabbitMQ');
-// const { CommentCreatedListener } = require('./events/publishers/comment-created-listener');
-const http = require('http');
-
+const connectDb = require('./db/connectDb');
+const router = require('./routes/posts');
+const { rabbitWrapper, connectRabbit } = require('@giantsoftnats/common')
+const { connect } = require('./connectRabbitMQ');
+const { CommentCreatedListener } = require('./events/publishers/comment-created-listener');
 const app = express();
 
 
-// connectDb();
+connectDb();
 
-// connectRabbit(rabbitWrapper, () => {
-//   new CommentCreatedListener(rabbitWrapper.client).listen();
-// })
-
-app.get('/api/posts/name', (req, res) => {
-  res.send('Hello World')
+connectRabbit(rabbitWrapper, () => {
+  new CommentCreatedListener(rabbitWrapper.client).listen();
 })
 
-// app.use(express.json());
+app.use(express.json());
 
-// app.use('/api/posts', router)
+app.use('/api/posts',router)
 
-const httpServer = http.createServer(app);
-
-httpServer.listen(3000, () => {
+app.listen(3000, () => {
   console.log(`Listening on port 3000`)
 })
